c9e5f195ca1db9499915bba2f35ea68287f52f27
Walkersneps <walkersneps@gmail.com>
2023-10-18 18:11:34 +0200

    Implement visualization demo

---
+++ remoteunit/main.py
@@ -0,0 +1,77 @@
+import tkinter as Tk
+from tkinter import ttk
+
+import screens as s
+
+import matplotlib.pyplot as plt
+plt.style.use('dark_background')
+
+def _quit():
+    window.quit()
+    window.destroy()
+
+def _makePage():
+    global pageframe, windowframe
+    pageframe = ttk.Frame(windowframe)
+    pageframe.grid(row= 0, column= 0)
+    print(currentPage)
+    screens[currentPage].build(pageframe)
+    screens[currentPage].animate()
+
+def nextPage():
+    global currentPage, nextpagebtn, prevpagebtn, screens, pageframe
+
+    screens[currentPage].stop()
+    currentPage += 1
+
+    # Check buttons availability
+    if currentPage >= (len(screens) - 1):
+        nextpagebtn.state(["disabled"])
+    if currentPage > 0:
+        prevpagebtn.state(["!disabled"])
+
+    # Destroy old page and build the new one
+    pageframe.destroy()
+    _makePage()
+
+def prevPage():
+    global currentPage, nextpagebtn, prevpagebtn, screens, pageframe
+
+    screens[currentPage].stop()
+    currentPage -= 1
+    
+     # Check buttons availability
+    if currentPage == 0:
+        prevpagebtn.state(["disabled"])
+    if currentPage < len(screens):
+        nextpagebtn.state(["!disabled"])
+
+    # Destroy old page and build the new one
+    pageframe.destroy()
+    _makePage()
+
+
+window = Tk.Tk() # Create Tcl interpreter + main window
+window.title("Live Data Monitor")
+window.protocol("WM_DELETE_WINDOW", _quit)
+
+windowframe = ttk.Frame(window, padding= 10) # Contains whole window
+windowframe.grid()
+#pageframe = ttk.Frame(windowframe) # Contains page-dependant top portion
+#pageframe.grid(row= 0, column= 0)
+footerframe = ttk.Frame(windowframe) # Contains footer
+footerframe.grid(row= 1, column= 0)
+
+# Initialize screens
+currentPage = 0
+screens = [s.Screen1(),
+           s.Screen2()]
+
+closebtn = ttk.Button(footerframe, text= "Quit", command= _quit).grid(row= 0, column= 0)
+prevpagebtn = ttk.Button(footerframe, text= "<--", command= prevPage)
+prevpagebtn.grid(row= 0, column= 2)
+nextpagebtn = ttk.Button(footerframe, text= "-->", command= nextPage)
+nextpagebtn.grid(row= 0, column= 3)
+
+_makePage()
+Tk.mainloop()
---
+++ remoteunit/screens.py
@@ -0,0 +1,90 @@
+from tkinter import ttk
+from itertools import cycle
+import random
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+class Screen1:
+
+    def build(self, container):
+        print("build s1")
+
+        self.frame = container
+        # Setup Plots
+        self.x_vals = [t for t in range(0, 121)]
+        self.y_vals = [0 for t in range(0, len(self.x_vals))]
+        self.y_vals2 = self.y_vals
+        self.c = cycle(self.x_vals)
+        plt.close()
+        plt.gcf().subplots(2, 1)
+
+        # Draw GUI
+        self.frame.grid()
+        label = ttk.Label(self.frame, text= "Pagina 1").grid(column= 0, row= 0)
+
+        self.canvas = FigureCanvasTkAgg(plt.gcf(), master= self.frame)
+        self.canvas.get_tk_widget().grid(column= 0, row= 1)
+
+    def _animateFrame(self, _):
+        # Generate values
+        i = next(self.c)
+        self.y_vals[i] = random.randint(0, 5)
+        self.y_vals2[i] = random.randint(0, 5)
+        # Get all axes of figure
+        ax1, ax2 = plt.gcf().get_axes()
+        # Clear current data
+        ax1.cla()
+        ax2.cla()
+        # Plot new data
+        ax1.plot(self.x_vals, self.y_vals)
+        ax2.plot(self.x_vals, self.y_vals2)
+
+    def animate(self):
+        self.anim = FuncAnimation(plt.gcf(), self._animateFrame, interval= 16, blit= False, frames= [0, 1])
+    
+    def stop(self):
+        del self.anim
+        del self.canvas
+
+
+class Screen2:
+
+    def build(self, container):
+        print("build s2")
+
+        self.frame = container
+        # Setup Plots
+        self.x_vals = [t for t in range(0, 121)]
+        self.y_vals = [0 for t in range(0, len(self.x_vals))]
+        self.c = cycle(self.x_vals)
+        plt.close()
+        plt.gcf().subplots(1, 1)
+
+        # Draw GUI
+        self.frame.grid()
+        label = ttk.Label(self.frame, text= "Pagina 2").grid(column= 0, row= 0)
+
+        self.canvas = FigureCanvasTkAgg(plt.gcf(), master= self.frame)
+        self.canvas.get_tk_widget().grid(column= 0, row= 1)
+
+    def _animateFrame(self, _):
+        # Generate values
+        i = next(self.c)
+        self.y_vals[i] = random.randint(0, 6)
+        # Get all axes of figure
+        ax1 = plt.gcf().get_axes()
+        ax1 = ax1[0]
+        # Clear current data
+        ax1.cla()
+        # Plot new data
+        ax1.plot(self.x_vals, self.y_vals)
+
+    def animate(self):
+        self.anim = FuncAnimation(plt.gcf(), self._animateFrame, interval= 16, blit= False, frames= [0, 1])
+    
+    def stop(self):
+        del self.anim
+        del self.canvas
